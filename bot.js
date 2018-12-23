30eb7823fdb3c025d85e7c22e8e24550983e5189
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-12-23 17:10:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`-bc | -obc | -ebc `,"http://twitch.tv/idk")
+client.user.setGame(`$bc | $obc | $ebc `,"http://twitch.tv/idk")
 client.user.setStatus("dnd")
 });
 
